398be1ee26b547ea7bc43e906d48a4a2c12961b9
samuel <legosthes@gmail.com>
2025-09-05 00:53:09 +0800

    feat: 增加其他必要設定和安裝能允許CORS的套件

---
+++ chrome-extension/popup/rewardia.js
@@ -0,0 +1,13 @@
+const token_url = "http://localhost:8000/users/api/get-token";
+
+await fetch(token_url, {
+  method: "GET",
+  credentials: "include",
+  header: {
+    "Content-Type": "application/json",
+  },
+})
+  .then((resp) => resp.json())
+  .then((data) => {
+    console.log(data);
+  });
