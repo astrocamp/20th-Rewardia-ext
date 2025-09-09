64a4831500605ac9c6d29a8e4ce47aa1b51a42ab
samuel <legosthes@gmail.com>
2025-09-10 00:28:01 +0800

    feat: 讓fetch到的卡片顯示在插件上

--- chrome-extension/popup/scripts/rewards.js
+++ chrome-extension/popup/scripts/rewards.js
@@ -1,8 +1,37 @@
-async function get_rewards(merchant) {
-  const url = `http://localhost:8000/api/rewards/${merchant}`;
+const rewards_content = document.querySelector(".rewards_content");
 
-  const response = await fetch(get_rewards);
+async function get_rewards(merchant) {
+  const url = `http://localhost:8000/api/rewards/scope/${merchant}`;
+
+  const response = await fetch(url);
   const data = await response.json();
 
   return data;
+}
+
+async function get_current_url() {
+  let queryOptions = { active: true, currentWindow: true };
+  const [tab] = await chrome.tabs.query(queryOptions);
+  return tab.url;
+}
+
+async function return_cards(merchant) {
+  const cards = await get_rewards(merchant);
+
+  cards.forEach((card) => {
+    const card_reward = `<div class="reward">
+        <div class="reward_text">
+          <h2>${card.card.name}</h2>
+          <span>最高${card.max_rate}%回饋</span>
+        </div>
+      </div>`;
+    console.log(card_reward);
+    rewards_content.insertAdjacentHTML("beforeend", card_reward);
+  });
+}
+
+const cur_url = await get_current_url();
+
+if (cur_url.includes("momo")) {
+  await return_cards("momo購物");
 }
