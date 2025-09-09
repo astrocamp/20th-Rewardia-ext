aa74d299fff971840439d93f756a2ca21d5f4304
samuel <legosthes@gmail.com>
2025-09-09 18:58:01 +0800

    feat: 增加回饋的頁面並測試抓取回饋的API

--- chrome-extension/popup/rewards.js
+++ chrome-extension/popup/rewards.js
@@ -1,4 +1,4 @@
-const get_rewards = "http://localhost:8000/users/api/get_rewards";
+const get_rewards = "http://localhost:8000/api/rewards/";
 
 const response = await fetch(get_rewards);
 const data = await response.json();
