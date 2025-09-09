3e2fa50a4279772c3275f7fd5d83034db224a8e9
samuel <legosthes@gmail.com>
2025-09-09 19:21:06 +0800

    feat: 增加momo的判斷

--- chrome-extension/content_scripts/content_script.js
+++ chrome-extension/content_scripts/content_script.js
@@ -6,23 +6,36 @@ if (window.location.hostname.includes("24h.pchome.com.tw")) {
   }
 }
 
+const current_host = window.location.hostname;
+
+if (
+  current_host == "24h.pchome.com.tw" ||
+  current_host == "www.momoshop.com.tw"
+) {
+  const close_popup = sessionStorage.getItem("close_popup");
+
+  if (!close_popup) {
+    show_rewardia_popup();
+  }
+}
+
 function show_rewardia_popup() {
   // Simple popup for testing
   const popup = document.createElement("div");
   popup.className = "popup";
-  popup.innerHTML = `<div class="header">
-  <div class="logo">REWARDIA</div>
-  <div class="close_btn">X</div>
+  popup.innerHTML = `<div class="popup_header">
+  <div class="popup_logo">REWARDIA</div>
+  <div class="popup_close_btn">X</div>
   </div>
-  <div class="center_img">
+  <div class="popup_center_img">
     <img src="${chrome.runtime.getURL("images/treasure.png")}" alt="" />
   </div>
-  <div class="text">
+  <div class="popup_text">
     <button class="popup_btn">偵測到信用卡，由此詳細</button>
   </div>`;
 
   document.body.appendChild(popup);
-  const close_btn = popup.querySelector(".close_btn");
+  const close_btn = popup.querySelector(".popup_close_btn");
   close_btn.addEventListener("click", function () {
     popup.remove();
     sessionStorage.setItem("close_popup", "true");
