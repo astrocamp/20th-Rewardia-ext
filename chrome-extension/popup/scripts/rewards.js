5df33fc7d236f7263383ea9da815549b2ca43b10
samuel <legosthes@gmail.com>
2025-09-11 17:31:52 +0800

    feat: 新增國內與海外信用卡回饋排名

--- chrome-extension/popup/scripts/rewards.js
+++ chrome-extension/popup/scripts/rewards.js
@@ -20,14 +20,34 @@ let card_count;
 async function return_cards(merchant) {
   const cards = await get_rewards(merchant);
 
+  const title = `<h1>${merchant}信用卡回饋</h1>`;
+  rewards_content.insertAdjacentHTML("beforeend", title);
+
   cards.forEach((card) => {
-    const card_reward = `<div class="reward">
+    let card_reward;
+    if (card.min_rate && card.max_rate) {
+      card_reward = `<div class="reward">
         <div class="reward_text">
           <h2>${card.card.name}</h2>
-          <span>最高${card.max_rate}%回饋</span>
+          <span>最高 ${card.max_rate}% 回饋</span>
+          <span>最低 ${card.min_rate}% 回饋</span>
         </div>
       </div>`;
-
+    } else if (card.min_rate == null) {
+      card_reward = `<div class="reward">
+        <div class="reward_text">
+          <h2>${card.card.name}</h2>
+          <span>最高 ${card.max_rate}% 回饋</span>
+        </div>
+      </div>`;
+    } else if (card.max_rate == null) {
+      card_reward = `<div class="reward">
+        <div class="reward_text">
+          <h2>${card.card.name}</h2>
+          <span>最低 ${card.min_rate}% 回饋</span>
+        </div>
+      </div>`;
+    }
     rewards_content.insertAdjacentHTML("beforeend", card_reward);
   });
 }
@@ -36,8 +56,10 @@ const cur_url = await get_current_url();
 
 if (cur_url.includes("momo")) {
   await return_cards("momo購物");
-}
-
-if (cur_url.includes("pchome")) {
+} else if (cur_url.includes("pchome")) {
   await return_cards("pchome");
+} else if (cur_url.includes("tw")) {
+  await return_cards("國內");
+} else {
+  await return_cards("海外");
 }
